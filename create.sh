208c66e87a68230f89c8cef172c215878fc98eb2
John ORaw <49677015+JohnORaw@users.noreply.github.com>
2024-05-12 10:43:38 +0100

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,1 +1,10 @@
 
+#!/bin/bash
+
+clear
+echo "**********************************************"
+echo This batch file will create a project directory
+echo "**********************************************"
+echo *** press [ctrl][c] to exit or any key to continue ***
+read
+
